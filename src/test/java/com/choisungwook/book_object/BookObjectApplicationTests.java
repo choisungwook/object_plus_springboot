c65842d83cfa7935a2387107242f3fe0ddf8511e
choisungwook <kgg1959@naver.com>
2021-08-17 20:11:39 +0900

    프로젝트 초기화

---
+++ src/test/java/com/choisungwook/book_object/BookObjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choisungwook.book_object;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookObjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
